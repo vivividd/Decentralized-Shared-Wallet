5c24d3b1ca5706dde9930a193bc42783d958d90a
timur <tim.tamoev@gmail.com>
2026-07-15 01:21:18 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Shared Wallet 
+It is a Web3 app that allows multiple people to approve a transaction before actually pulling it off
---
+++ src/SharedWallet.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract SharedWallet{
+  address [] public owners; 
+  uint16 public requiredApprovals; 
+  mapping(address => bool) public isOwner;
+
+  constructor (address [] memory _owners, uint16 _requiredApprovals) {
+    require(_owners.length> 0, "At least one owner must exist. Currently - 0.");
+
+    require (
+      _owners.length >= _requiredApprovals &&
+      _requiredApprovals > 0, 
+      "Invalid Approval Requirement."
+    );
+
+    for (uint256 i = 0; i < _owners.length; i++) {
+      address owner = _owners[i];
+
+      require(owner != address(0), "Invalid Owner Address.");
+      require(!isOwner[owner], "Duplicated Owner."); // Check if the address is already added.
+      
+      isOwner[owner] = true;
+      owners.push(owner);
+    }
+
+    requiredApprovals = _requiredApprovals;
+  }
+}
