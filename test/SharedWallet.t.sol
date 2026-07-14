a23046efb277df5ceac6b63c07bfc975995c8d94
Tim <tim.tamoev@gmail.com>
2026-07-15 02:07:29 +0300

    Switch solc version so it works + recreate testing template a bit

--- test/SharedWallet.t.sol
+++ test/SharedWallet.t.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.36;
+pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 import {SharedWallet} from "../src/SharedWallet.sol";
 
 contract TestSharedWallet is Test {
-  function testConstructor(address [] memory _owners, uint16 _requiredApprovals) public {
-    // I've got nothin so far..   
+  SharedWallet wallet;
+
+  function setUp() public { 
+    //wallet = new SharedWallet(); 
   }
 }
