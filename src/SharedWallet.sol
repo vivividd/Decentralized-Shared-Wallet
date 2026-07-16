7555de2213c8d1002211a4bd1660476823bcdd0a
Tim <tim.tamoev@gmail.com>
2026-07-16 14:20:05 +0300

    formatted it

--- src/SharedWallet.sol
+++ src/SharedWallet.sol
@@ -1,33 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-contract SharedWallet{
-  address [] public owners; 
-  uint16 public requiredApprovals; 
-  mapping(address => bool) public isOwner;
-  
-  error InvalidOwnersRequirement();
-  error InvalidApprovalRequirement();
+contract SharedWallet {
+    address[] public owners;
+    uint16 public requiredApprovals;
+    mapping(address => bool) public isOwner;
 
-  constructor (address [] memory _owners, uint16 _requiredApprovals) {
-    if(_owners.length == 0) { revert InvalidOwnersRequirement(); }
-  
-    if (
-      _owners.length < _requiredApprovals ||
-      _requiredApprovals <= 0
-    ) { revert InvalidApprovalRequirement(); }
+    error InvalidOwnersRequirement();
+    error InvalidApprovalRequirement();
 
-    for (uint256 i = 0; i < _owners.length; i++) {
-      address owner = _owners[i];
-      
-      if (owner == address(0)) {revert InvalidOwnersRequirement(); }
+    constructor(address[] memory _owners, uint16 _requiredApprovals) {
+        if (_owners.length == 0) revert InvalidOwnersRequirement();
 
-      if(isOwner[owner]) { revert InvalidOwnersRequirement(); }
-      
-      isOwner[owner] = true;
-      owners.push(owner);
+        if (_owners.length < _requiredApprovals || _requiredApprovals <= 0) revert InvalidApprovalRequirement();
+
+        for (uint256 i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+
+            if (owner == address(0)) revert InvalidOwnersRequirement();
+
+            if (isOwner[owner]) revert InvalidOwnersRequirement();
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        requiredApprovals = _requiredApprovals;
     }
-
-    requiredApprovals = _requiredApprovals;
-  }
 }
--- test/SharedWallet.t.sol
+++ test/SharedWallet.t.sol
@@ -5,81 +5,65 @@ import {Test} from "forge-std/Test.sol";
 import {SharedWallet} from "../src/SharedWallet.sol";
 
 contract TestSharedWallet is Test {
-  
-  // CONSTRUCTOR testing
+    // CONSTRUCTOR testing
 
-  function testWrongOwnerAddress(
-    address [] memory _owners, 
-    uint16 _requiredApprovals) public {
-      vm.assume(_owners.length >= _requiredApprovals);
-      vm.assume(_requiredApprovals > 0);
+    function testWrongOwnerAddress(address[] memory _owners, uint16 _requiredApprovals) public {
+        vm.assume(_owners.length >= _requiredApprovals);
+        vm.assume(_requiredApprovals > 0);
 
-      _owners[0] = address(0);
-      for(uint256 i = 1; i < _owners.length; i++) {
-        vm.assume(_owners[i] != address(0)); 
-      }
-    
-      vm.expectRevert(SharedWallet.InvalidOwnersRequirement.selector);
+        _owners[0] = address(0);
+        for (uint256 i = 1; i < _owners.length; i++) {
+            vm.assume(_owners[i] != address(0));
+        }
 
-      new SharedWallet(_owners, _requiredApprovals);
+        vm.expectRevert(SharedWallet.InvalidOwnersRequirement.selector);
+
+        new SharedWallet(_owners, _requiredApprovals);
     }
 
-  function testZeroOwners(
-    uint16 _requiredApprovals) public {
-      address [] memory emptyOwners = new address [](0);
-      vm.expectRevert(SharedWallet.InvalidOwnersRequirement.selector);
+    function testZeroOwners(uint16 _requiredApprovals) public {
+        address[] memory emptyOwners = new address[](0);
+        vm.expectRevert(SharedWallet.InvalidOwnersRequirement.selector);
 
-      new SharedWallet(emptyOwners, _requiredApprovals);
-  }
-  
-  function testZeroApprovals(
-    address [] memory _owners
-    ) public {
-      
-      vm.assume(_owners.length > 0);
-      for(uint256 i = 0; i < _owners.length; i++) {
-        vm.assume(_owners[i] != address(0)); 
-      }
+        new SharedWallet(emptyOwners, _requiredApprovals);
+    }
 
-      vm.expectRevert(SharedWallet.InvalidApprovalRequirement.selector);
+    function testZeroApprovals(address[] memory _owners) public {
+        vm.assume(_owners.length > 0);
+        for (uint256 i = 0; i < _owners.length; i++) {
+            vm.assume(_owners[i] != address(0));
+        }
 
-      new SharedWallet(_owners, 0);
-  }
+        vm.expectRevert(SharedWallet.InvalidApprovalRequirement.selector);
 
-  function testLessOwnersThanApprovals(
-    uint16 _requiredApprovals,
-    address [] memory _owners
-    ) public {
-      
-      vm.assume(_owners.length < _requiredApprovals);
-      vm.assume(_requiredApprovals > 0);
+        new SharedWallet(_owners, 0);
+    }
 
-      vm.assume(_owners.length > 0);
-      for(uint256 i = 0; i < _owners.length; i++) {
-        vm.assume(_owners[i] != address(0)); 
-      }
+    function testLessOwnersThanApprovals(uint16 _requiredApprovals, address[] memory _owners) public {
+        vm.assume(_owners.length < _requiredApprovals);
+        vm.assume(_requiredApprovals > 0);
 
-      vm.expectRevert(SharedWallet.InvalidApprovalRequirement.selector);
+        vm.assume(_owners.length > 0);
+        for (uint256 i = 0; i < _owners.length; i++) {
+            vm.assume(_owners[i] != address(0));
+        }
 
-      new SharedWallet(_owners, 0);
-  }
+        vm.expectRevert(SharedWallet.InvalidApprovalRequirement.selector);
 
+        new SharedWallet(_owners, 0);
+    }
 
-  function testDuplicatedOwners(
-    uint16 _requiredApprovals,
-    address [] memory _owners
-    ) public {
-      
-      vm.assume(_owners.length > 2);
-      vm.assume(_requiredApprovals >= 2);
-      vm.assume(_owners.length >= _requiredApprovals);
+    function testDuplicatedOwners(uint16 _requiredApprovals, address[] memory _owners) public {
+        vm.assume(_owners.length > 2);
+        vm.assume(_requiredApprovals >= 2);
+        vm.assume(_owners.length >= _requiredApprovals);
 
-      for(uint256 i = 0; i < _owners.length; i++) {
-        vm.assume(_owners[i] != address(0)); 
-      }
-      _owners[0] = _owners[1];
-      vm.expectRevert(SharedWallet.InvalidOwnersRequirement.selector);
+        for (uint256 i = 0; i < _owners.length; i++) {
+            vm.assume(_owners[i] != address(0));
+        }
+        _owners[0] = _owners[1];
+        vm.expectRevert(SharedWallet.InvalidOwnersRequirement.selector);
 
-      new SharedWallet(_owners, _requiredApprovals);
-  }
+        new SharedWallet(_owners, _requiredApprovals);
+    }
 }
