eaf179e0734cb45107292447c904e34a9316b32f
Tim <tim.tamoev@gmail.com>
2026-07-18 21:51:19 +0300

    Add more tests, add key functions.

--- src/SharedWallet.sol
+++ src/SharedWallet.sol
@@ -2,12 +2,51 @@
 pragma solidity ^0.8.24;
 
 contract SharedWallet {
+    struct Transaction {
+        address to;
+        uint256 value;
+        bytes data;
+        bool executed;
+        uint256 approvalCount;
+    }
+
     address[] public owners;
     uint16 public requiredApprovals;
     mapping(address => bool) public isOwner;
+    Transaction[] public transactions;
+    mapping(uint256 => mapping(address => bool)) public approved;
 
     error InvalidOwnersRequirement();
     error InvalidApprovalRequirement();
+    error NotOwner();
+    error InvalidRecipient();
+    error TransactionDoesNotExist();
+    error TransactionAlreadyExecuted();
+    error TransactionAlreadyApproved();
+    error TransactionNotApproved();
+    error NotEnoughApprovals();
+    error TransactionExecutionFailed();
+
+    event Deposit(address indexed sender, uint256 amount, uint256 balance);
+    event TransactionSubmitted(uint256 indexed transactionId, address indexed to, uint256 value, bytes data);
+    event TransactionApproved(uint256 indexed transactionId, address indexed owner);
+    event TransactionApprovalRevoked(uint256 indexed transactionId, address indexed owner);
+    event TransactionExecuted(uint256 indexed transactionId);
+
+    modifier onlyOwner() {
+        if (!isOwner[msg.sender]) revert NotOwner();
+        _;
+    }
+
+    modifier transactionExists(uint256 transactionId) {
+        if (transactionId >= transactions.length) revert TransactionDoesNotExist();
+        _;
+    }
+
+    modifier notExecuted(uint256 transactionId) {
+        if (transactions[transactionId].executed) revert TransactionAlreadyExecuted();
+        _;
+    }
 
     constructor(address[] memory _owners, uint16 _requiredApprovals) {
         if (_owners.length == 0) revert InvalidOwnersRequirement();
@@ -26,5 +65,63 @@ contract SharedWallet {
         }
 
         requiredApprovals = _requiredApprovals;
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+
+    function submitTransaction(address to, uint256 value, bytes calldata data)
+        external
+        onlyOwner
+        returns (uint256 transactionId)
+    {
+        if (to == address(0)) revert InvalidRecipient();
+
+        transactionId = transactions.length;
+        transactions.push(Transaction({to: to, value: value, data: data, executed: false, approvalCount: 0}));
+        emit TransactionSubmitted(transactionId, to, value, data);
+    }
+
+    function approveTransaction(uint256 transactionId)
+        external
+        onlyOwner
+        transactionExists(transactionId)
+        notExecuted(transactionId)
+    {
+        if (approved[transactionId][msg.sender]) revert TransactionAlreadyApproved();
+
+        approved[transactionId][msg.sender] = true;
+        transactions[transactionId].approvalCount++;
+        emit TransactionApproved(transactionId, msg.sender);
+    }
+
+    function revokeApproval(uint256 transactionId)
+        external
+        onlyOwner
+        transactionExists(transactionId)
+        notExecuted(transactionId)
+    {
+        if (!approved[transactionId][msg.sender]) revert TransactionNotApproved();
+
+        approved[transactionId][msg.sender] = false;
+        transactions[transactionId].approvalCount--;
+        emit TransactionApprovalRevoked(transactionId, msg.sender);
+    }
+
+    function executeTransaction(uint256 transactionId)
+        external
+        onlyOwner
+        transactionExists(transactionId)
+        notExecuted(transactionId)
+    {
+        Transaction storage transaction = transactions[transactionId];
+        if (transaction.approvalCount < requiredApprovals) revert NotEnoughApprovals();
+
+        transaction.executed = true;
+        (bool success,) = transaction.to.call{value: transaction.value}(transaction.data);
+        if (!success) revert TransactionExecutionFailed();
+
+        emit TransactionExecuted(transactionId);
     }
 }
--- test/SharedWallet.t.sol
+++ test/SharedWallet.t.sol
@@ -4,7 +4,28 @@ pragma solidity ^0.8.24;
 import {Test} from "forge-std/Test.sol";
 import {SharedWallet} from "../src/SharedWallet.sol";
 
+contract RevertingTarget {
+    fallback() external payable {
+        revert();
+    }
+}
+
 contract TestSharedWallet is Test {
+    address owner1 = address(0x1);
+    address owner2 = address(0x2);
+    address owner3 = address(0x3);
+    address recipient = address(0x4);
+    SharedWallet wallet;
+
+    function setUp() public {
+        address[] memory walletOwners = new address[](3);
+        walletOwners[0] = owner1;
+        walletOwners[1] = owner2;
+        walletOwners[2] = owner3;
+        wallet = new SharedWallet(walletOwners, 2);
+        vm.deal(address(wallet), 10 ether);
+    }
+
     // CONSTRUCTOR testing
 
     function testWrongOwnerAddress(address[] memory _owners, uint16 _requiredApprovals) public {
@@ -41,7 +62,7 @@ contract TestSharedWallet is Test {
 
     function testLessOwnersThanApprovals(uint16 _requiredApprovals, address[] memory _owners) public {
         vm.assume(_owners.length < _requiredApprovals);
-        vm.assume(_requiredApprovals > 0);
+        vm.assume(_requiredApprovals > _owners.length);
 
         vm.assume(_owners.length > 0);
         for (uint256 i = 0; i < _owners.length; i++) {
@@ -50,7 +71,7 @@ contract TestSharedWallet is Test {
 
         vm.expectRevert(SharedWallet.InvalidApprovalRequirement.selector);
 
-        new SharedWallet(_owners, 0);
+        new SharedWallet(_owners, _requiredApprovals);
     }
 
     function testDuplicatedOwners(uint16 _requiredApprovals, address[] memory _owners) public {
@@ -65,5 +86,103 @@ contract TestSharedWallet is Test {
         vm.expectRevert(SharedWallet.InvalidOwnersRequirement.selector);
 
         new SharedWallet(_owners, _requiredApprovals);
+    }
+
+    function testSubmitTransaction() public {
+        bytes memory data = abi.encodeWithSignature("receiveFunds()");
+
+        vm.prank(owner1);
+        uint256 transactionId = wallet.submitTransaction(recipient, 1 ether, data);
+
+        (address to, uint256 value, bytes memory storedData, bool executed, uint256 approvalCount) =
+            wallet.transactions(transactionId);
+        assertEq(to, recipient);
+        assertEq(value, 1 ether);
+        assertEq(storedData, data);
+        assertFalse(executed);
+        assertEq(approvalCount, 0);
+    }
+
+    function testNonOwnerCannotSubmitOrApprove() public {
+        vm.prank(address(0x99));
+        vm.expectRevert(SharedWallet.NotOwner.selector);
+        wallet.submitTransaction(recipient, 0, "");
+
+        vm.prank(address(0x99));
+        vm.expectRevert(SharedWallet.NotOwner.selector);
+        wallet.approveTransaction(0);
+    }
+
+    function testCannotApproveTwiceAndCanRevoke() public {
+        vm.prank(owner1);
+        wallet.submitTransaction(recipient, 1 ether, "");
+
+        vm.prank(owner1);
+        wallet.approveTransaction(0);
+        (,,,, uint256 approvalCount) = wallet.transactions(0);
+        assertEq(approvalCount, 1);
+
+        vm.prank(owner1);
+        vm.expectRevert(SharedWallet.TransactionAlreadyApproved.selector);
+        wallet.approveTransaction(0);
+
+        vm.prank(owner1);
+        wallet.revokeApproval(0);
+        (,,,, approvalCount) = wallet.transactions(0);
+        assertEq(approvalCount, 0);
+
+        vm.prank(owner1);
+        vm.expectRevert(SharedWallet.TransactionNotApproved.selector);
+        wallet.revokeApproval(0);
+    }
+
+    function testExecutesAfterRequiredApprovals() public {
+        vm.prank(owner1);
+        wallet.submitTransaction(recipient, 1 ether, "");
+
+        vm.prank(owner1);
+        wallet.approveTransaction(0);
+        vm.prank(owner2);
+        wallet.approveTransaction(0);
+
+        uint256 recipientBalanceBefore = recipient.balance;
+        vm.prank(owner3);
+        wallet.executeTransaction(0);
+
+        assertEq(recipient.balance, recipientBalanceBefore + 1 ether);
+        (,,, bool executed,) = wallet.transactions(0);
+        assertTrue(executed);
+
+        vm.prank(owner1);
+        vm.expectRevert(SharedWallet.TransactionAlreadyExecuted.selector);
+        wallet.executeTransaction(0);
+    }
+
+    function testCannotExecuteWithoutEnoughApprovals() public {
+        vm.prank(owner1);
+        wallet.submitTransaction(recipient, 1 ether, "");
+        vm.prank(owner1);
+        wallet.approveTransaction(0);
+
+        vm.prank(owner1);
+        vm.expectRevert(SharedWallet.NotEnoughApprovals.selector);
+        wallet.executeTransaction(0);
+    }
+
+    function testExecutionFailureDoesNotConsumeTransaction() public {
+        RevertingTarget target = new RevertingTarget();
+        vm.prank(owner1);
+        wallet.submitTransaction(address(target), 0, "");
+        vm.prank(owner1);
+        wallet.approveTransaction(0);
+        vm.prank(owner2);
+        wallet.approveTransaction(0);
+
+        vm.prank(owner1);
+        vm.expectRevert(SharedWallet.TransactionExecutionFailed.selector);
+        wallet.executeTransaction(0);
+
+        (,,, bool executedAfterFailure,) = wallet.transactions(0);
+        assertFalse(executedAfterFailure);
     }
 }
